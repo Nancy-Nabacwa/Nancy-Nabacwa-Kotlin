e13bc59841c39c347894cb7d8fce33b606ac6b1d
nancy pauline nabacwa <nancypaulinenabacwa@gmail.com>
2024-02-06 22:47:44 +0300

    added new kotlin file

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+   var fullName  = "Nancy Nabacwa"
+
+   var age = 22
+
+    var phoneNumber = 705553284
+
+    var weight = "64.5kg"
+
+    var citizen = "Kenyan"
+
+    val studentKenyan = true
+    val studentNotKenyan = false
+
+
+
+}
